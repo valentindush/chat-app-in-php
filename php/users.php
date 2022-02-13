f9a63d6838bd7b00528c6ac31156cd0959d37c53
Valentin <93974733+valentindush@users.noreply.github.com>
2022-02-13 12:33:32 +0200

    Update users.php

--- php/users.php
+++ php/users.php
@@ -5,7 +5,7 @@ session_start();
     $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ");
     $output = "";
 
-    if(mysqli_num_rows($sql) == 1){
+    if(mysqli_num_rows($sql) < 1){
         $output .= "no users available";
     }elseif(mysqli_num_rows($sql) > 0){
        include "data.php";
